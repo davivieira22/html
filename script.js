0da3261af239cd81d5efa18fdcc3a9884a8dade6
davivieira22 <nftdavidavila22@gmail.com>
2024-03-30 21:09:07 -0300

    conversor de moedas

--- script.js
+++ script.js
@@ -2,49 +2,44 @@ const convertButton = document.querySelector(".convert-button")
 const currencySelect = document.querySelector(".currency-select")
 
 
-function convertValue() {
+async function convertValue() {
     const inputCurrecyValue = document.querySelector(".input-value").value
     const valueToConvert = document.querySelector(".valor-real")//valor em real
     const valueConvert = document.querySelector(".valor")//outras moedas
 
+    const date = await fetch(" https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL").then(response => response.json())
 
-    const dolarToday = 4.94
-    const euroToday = 5.34
-    const libraToday = 6.24
-    const btcToday = 51
-
+    const dolar = date.EURBRL.high
+    const euro = date.EURBRL.high
+    const libra = 6.33
+    const btc = date.BTCBRL.high
     if (currencySelect.value == "dolar") {
         valueConvert.innerHTML = new Intl.NumberFormat("en-US", {
             style: "currency", currency: "USD"
-        }).format(inputCurrecyValue / dolarToday)
+        }).format(inputCurrecyValue / dolar)
     }
 
     if (currencySelect.value == "euro") {
         valueConvert.innerHTML =
             new Intl.NumberFormat("de-DE", {
                 style: "currency", currency: "EUR"
-            }).format(inputCurrecyValue / euroToday)
+            }).format(inputCurrecyValue / euro)
     }
     if (currencySelect.value == "libra") {
-        valueConvert.innerHTML = new Intl.NumberFormat("en-UK", {
+        valueConvert.innerHTML = new Intl.NumberFormat("ez-UK", {
             style: "currency", currency: "GBP"
-        }).format(inputCurrecyValue / libraToday)
+        }).format(inputCurrecyValue / libra)
     }
     if (currencySelect.value == "btc") {
-        valueConvert.innerHTML = new Intl.NumberFormat("en-US", {
-            style: "currency", currency: "USD "
-        }).format(inputCurrecyValue / btcToday)
+        valueConvert.innerHTML = new Intl.NumberFormat("es-SV", {
+            style: "currency", currency: "BTC "
+        }).format(inputCurrecyValue / btc)
     }
 
     valueToConvert.innerHTML = new Intl.NumberFormat("pt-br", {
         style: "currency", currency: "BRL"
     }).format(inputCurrecyValue)
 }
-
-
-
-
-
 
 function currencyChange() {
     const currencyName = document.getElementById("currency-valor")
@@ -62,10 +57,10 @@ function currencyChange() {
         currencyName.innerHTML = "libra"
         currencyImg.src = "./assets/libras.png"
     }
-if(currencySelect.value == "btc"){
-    currencyName.innerHTML=" bitcoin"
-    currencyImg.src ="./assets/bitcoin 1.png"
-}
+    if (currencySelect.value == "btc") {
+        currencyName.innerHTML = " bitcoin"
+        currencyImg.src = "./assets/bitcoin 1.png"
+    }
     convertValue()
 
 }
